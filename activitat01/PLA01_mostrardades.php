1e18a965d90dde46615bdad9fc87d2c0f380baca
eina <109956262+encarbassotnopot@users.noreply.github.com>
2025-07-10 16:49:16 +0200

    catalanitzar a01

--- activitat01/PLA01_mostrardades.php
+++ activitat01/PLA01_mostrardades.php
@@ -1,13 +1,15 @@
 <?php
-	 
+
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>PLA01</title>
-	<link rel="stylesheet" type="text/css" href="css/estilos.css?v=1.0">
+	<link rel="stylesheet" type="text/css" href="css/estils.css?v=1.0">
 </head>
+
 <body>
 	<div class='container'>
 		<h1 class='centrar'>PLA01: MOSTRAR DADES</h1>
@@ -16,12 +18,12 @@
 			<input type="text" placeholder="nom" disabled value=''>
 			<input type="text" placeholder="cognoms" disabled value=''><br><br>
 			<input type="text" placeholder="qualificació" disabled value=''>
-			<!--aqui iran las cajitas <aside></aside>-->
 			<br><br>
 			<input type="text" placeholder="email" disabled value=''><br><br>
-			<textarea  cols='22' rows='5' disabled></textarea>
-			<p class='errores'></p>
+			<textarea cols='22' rows='5' disabled></textarea>
+			<p class='errors'></p>
 		</div>
 	</div>
 </body>
+
 </html>
